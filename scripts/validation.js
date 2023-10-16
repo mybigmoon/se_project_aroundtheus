c70f01cc754111b0be1571cd420e390c671b0e70
mybigmoon <amirahadla@icloud.com>
2023-10-16 13:43:22 -0400

    fix classes

--- scripts/validation.js
+++ scripts/validation.js
@@ -61,8 +61,8 @@ function enableValidation(options) {
 const config = {
   formSelector: ".modal__form",
   inputSelector: ".modal__input",
-  submitButtonSelector: ".modal__save-button",
-  inactiveButtonClass: "modal__button_disabled",
+  submitButtonSelector: ".modal__save_button",
+  inactiveButtonClass: "modal__save_button__disabled",
   inputErrorClass: "modal__input_type_error",
   errorClass: "modal__error_visible",
 };
