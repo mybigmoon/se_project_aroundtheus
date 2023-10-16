7f4e0aae9a20809eabf424bc19f20a96ded65f11
mybigmoon <amirahadla@icloud.com>
2023-10-16 13:54:05 -0400

    fix name of class

--- scripts/validation.js
+++ scripts/validation.js
@@ -62,7 +62,7 @@ const config = {
   formSelector: ".modal__form",
   inputSelector: ".modal__input",
   submitButtonSelector: ".modal__save-button",
-  inactiveButtonClass: "modal__save-button--disabled",
+  inactiveButtonClass: "modal__save-button_disabled",
   inputErrorClass: "modal__input_type_error",
   errorClass: "modal__error_visible",
 };
